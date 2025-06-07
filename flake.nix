2667bf330ef1e2a1324ee4962d14e4a5b1e68fdb
aCeTotal <lars.oksendal@gmail.com>
2025-06-08 00:14:52 +0200

    updating flake

--- flake.nix
+++ flake.nix
@@ -1,26 +1,48 @@
 {
-  description = "argonpkgs – just some derivations";
+  description = "argonpkgs – mine lokale derivations";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url     = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, ... }:
-    let
-      systems = [ "x86_64-linux" ];
-      pkgFiles = builtins.attrNames (builtins.readDir ./packages);
-    in
-    {
-      packages = builtins.listToAttrs (map (system: {
-        name = system;
-        value = let
-          pkgs = import nixpkgs { inherit system; };
-        in
-          builtins.listToAttrs (map (file: {
-            name  = builtins.replaceStrings [ ".nix" ] [ "" ] file;
-            value = import ./packages/${file} { inherit pkgs; };
-          }) pkgFiles);
-      }) systems);
-    };
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgFiles = builtins.attrNames (builtins.readDir ./packages);
+
+        myOverlay = final: prev:
+          builtins.listToAttrs (
+            map (file:
+              let
+                name = builtins.replaceStrings [".nix"] ["" ] file;
+                args = if file == "argon.nix"
+                        then { argonpkgs = final; }
+                        else {};
+                drv  = final.callPackage ./packages/${file} args;
+              in {
+                inherit name;
+                value = drv;
+              }
+            ) pkgFiles
+          );
+
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ myOverlay ];
+        };
+
+        # Eksponer pakke-settet under packages.<system>.<name>
+        pkgSet = builtins.listToAttrs (
+          map (file:
+            let name = builtins.replaceStrings [".nix"] ["" ] file;
+            in { inherit name; value = pkgs.${name}; }
+          ) pkgFiles
+        );
+      in
+      {
+        packages = pkgSet;
+      }
+    );
 }
 
